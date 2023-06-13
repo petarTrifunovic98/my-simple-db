0bd1e8a0363c9b42b4939e8b11d3ed520d544699
Petar Trifunovic <petar.trifunovic@42computing.tech>
2023-06-13 15:07:04 +0200

    Fixed node header structure packing

--- pkg/paging/node.go
+++ pkg/paging/node.go
@@ -3,6 +3,7 @@ package paging
 import (
 	"encoding/binary"
 	"fmt"
+	"unsafe"
 )
 
 type NodeType uint8
@@ -15,22 +16,15 @@ const (
 const NODE_HEADER_SIZE = 1 + 1 + 4 + 2 + 2 + 2 //add the sizes of the types used in NodeHeader struct
 
 type NodeHeader struct {
-	nodeType      NodeType
-	isRoot        bool
 	parent        uint32
 	numCells      uint16
 	totalBodySize uint16
 	keySize       uint16
+	nodeType      NodeType
+	isRoot        bool
 }
 
 func (nh *NodeHeader) Serialize() []byte {
-	nodeTypeBytes := byte(nh.nodeType)
-
-	isRootUint8 := uint8(0)
-	if nh.isRoot {
-		isRootUint8 = 1
-	}
-	isRootBytes := byte(isRootUint8)
 	parentBytes := make([]byte, 4)
 	binary.LittleEndian.PutUint32(parentBytes, nh.parent)
 
@@ -43,9 +37,16 @@ func (nh *NodeHeader) Serialize() []byte {
 	keySizeBytes := make([]byte, 2)
 	binary.LittleEndian.PutUint16(keySizeBytes, nh.keySize)
 
+	fmt.Println(unsafe.Sizeof(*nh))
+	nodeTypeBytes := byte(nh.nodeType)
+
+	isRootUint8 := uint8(0)
+	if nh.isRoot {
+		isRootUint8 = 1
+	}
+	isRootBytes := byte(isRootUint8)
+
 	nodeHeaderBytes := make([]byte, 0, NODE_HEADER_SIZE)
-	fmt.Println("Header byte len:", len(nodeHeaderBytes))
-	nodeHeaderBytes = append(nodeHeaderBytes, nodeTypeBytes, isRootBytes)
 	fmt.Println("Header byte len:", len(nodeHeaderBytes))
 	nodeHeaderBytes = append(nodeHeaderBytes, parentBytes...)
 	fmt.Println("Header byte len:", len(nodeHeaderBytes))
@@ -54,24 +55,24 @@ func (nh *NodeHeader) Serialize() []byte {
 	nodeHeaderBytes = append(nodeHeaderBytes, totalBodySizeBytes...)
 	fmt.Println("Header byte len:", len(nodeHeaderBytes))
 	nodeHeaderBytes = append(nodeHeaderBytes, keySizeBytes...)
-
+	fmt.Println("Header byte len:", len(nodeHeaderBytes))
+	nodeHeaderBytes = append(nodeHeaderBytes, nodeTypeBytes, isRootBytes)
 	fmt.Println("Header byte len:", len(nodeHeaderBytes))
 
 	return nodeHeaderBytes
 }
 
 func (nh *NodeHeader) Deserialize(nodeHeaderBytes []byte) {
-	nh.nodeType = NodeType(nodeHeaderBytes[0])
-	isRootBytes := nodeHeaderBytes[1]
+	nh.parent = binary.LittleEndian.Uint32(nodeHeaderBytes[0:4])
+	nh.numCells = binary.LittleEndian.Uint16(nodeHeaderBytes[4:6])
+	nh.totalBodySize = binary.LittleEndian.Uint16(nodeHeaderBytes[6:8])
+	nh.keySize = binary.LittleEndian.Uint16(nodeHeaderBytes[8:10])
+	nh.nodeType = NodeType(nodeHeaderBytes[10])
+	isRootBytes := nodeHeaderBytes[11]
 	nh.isRoot = true
 	if isRootBytes == 0 {
 		nh.isRoot = false
 	}
-
-	nh.parent = binary.LittleEndian.Uint32(nodeHeaderBytes[2:6])
-	nh.numCells = binary.LittleEndian.Uint16(nodeHeaderBytes[6:8])
-	nh.totalBodySize = binary.LittleEndian.Uint16(nodeHeaderBytes[8:10])
-	nh.keySize = binary.LittleEndian.Uint16(nodeHeaderBytes[10:12])
 }
 
 func (nh *NodeHeader) Print() {
