723003c772eee3681321e4f656a155003ae0b1c4
Petar Trifunović <peki.trifunovic@gmail.com>
2023-08-10 11:27:44 +0200

    Added functionality for converting a row into a more JSON-friendly representation

--- pkg/row/row.go
+++ pkg/row/row.go
@@ -1,17 +1,38 @@
 package row
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+)
 
 const USERNAME_LEN uint32 = 32
 const EMAIL_LEN uint32 = 256
 
 type Row struct {
-	Id       uint32
-	Username [USERNAME_LEN]byte
-	Email    [EMAIL_LEN]byte
+	Id       uint32             `json:"id"`
+	Username [USERNAME_LEN]byte `json:"username"`
+	Email    [EMAIL_LEN]byte    `json:"email"`
+}
+
+type RowDTO struct {
+	Id       uint32 `json:"id"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
 }
 
 func (r *Row) ToString() string {
 	return fmt.Sprintf("ID: %d, Username: %s, Email: %s", r.Id, string(r.Username[:]), string(r.Email[:]))
+}
 
+func (r *Row) ToRowDTO() *RowDTO {
+	usernameBefore, _, _ := bytes.Cut(r.Username[:], []byte{0})
+	emailBefore, _, _ := bytes.Cut(r.Email[:], []byte{0})
+
+	ret := &RowDTO{
+		Id:       r.Id,
+		Username: string(usernameBefore),
+		Email:    string(emailBefore),
+	}
+
+	return ret
 }
